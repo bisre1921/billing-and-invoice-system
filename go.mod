cb5fc79fb609e9336deb9fbcf533fbab43cba39c
Mussie Alemayehu <157012749+mussie-alemayehu@users.noreply.github.com>
2025-03-04 21:03:10 +0300

    hash passwords

--- go.mod
+++ go.mod
@@ -3,3 +3,4 @@ module github.com/bisre1921/billing-and-invoice-system
 go 1.23.0
 
 require github.com/google/uuid v1.6.0
+require golang.org/x/crypto v0.35.0
--- models/user.go
+++ models/user.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserID string
@@ -13,7 +14,7 @@ type User struct {
 	Name      string
 	Email     string
 	Phone     string
-	Password  string
+	Password  string `json:"-"` // exclude password from json responses
 	Address   string
 	CreatedAt time.Time
 	UpdatedAt time.Time
@@ -22,4 +23,17 @@ type User struct {
 
 func NewUserID() UserID {
 	return UserID(uuid.New().String())
+}
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
+}
+
+func CheckPassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
 }
