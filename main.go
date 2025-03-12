2e852669d9ec1f5390c61bea985dffec6d269156
bisre1921 <bisrattewodros3@gmail.com>
2025-03-12 20:32:01 +0300

    add cors middleware

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	"github.com/bisre1921/billing-and-invoice-system/config"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,4 +19,13 @@ func main() {
 		fmt.Println("Database connection established successfully!")
 	}
 
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 }
