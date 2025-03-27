a30447cdf3a92f3bc5214c86ea749c23dfb32c35
bisre1921 <bisrattewodros3@gmail.com>
2025-03-27 22:55:27 +0300

    add CreateCompany controller

---
+++ controllers/company.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/bisre1921/billing-and-invoice-system/config"
+	"github.com/bisre1921/billing-and-invoice-system/models"
+	"github.com/gin-gonic/gin"
+)
+
+// CreateCompany godoc
+// @Summary Creates a new company
+// @Description Registers a new company in the system
+// @Tags Company
+// @Accept json
+// @Produce json
+// @Param company body models.Company true "Company Data"
+// @Success 201 {object} map[string]interface{} "Company created successfully"
+// @Failure 400 {object} map[string]interface{} "Bad Request"
+// @Failure 500 {object} map[string]interface{} "Internal Server Error"
+// @Router /company/create [post]
+func CreateCompany(c *gin.Context) {
+	var company models.Company
+	if err := c.ShouldBindJSON(&company); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	company.CreatedAt = time.Now()
+	company.UpdatedAt = time.Now()
+
+	result, err := config.DB.Collection("companies").InsertOne(context.Background(), company)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Company created successfully", "company_id": result.InsertedID})
+}
