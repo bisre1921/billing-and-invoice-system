00d47de3c8de5c104ec8e9c8d2f035d56172131d
bisre1921 <bisrattewodros3@gmail.com>
2025-04-21 09:56:41 +0300

    implement the report model

---
+++ models/report.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Report struct {
+	ID               primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	CompanyID        string             `json:"company_id"`
+	Title            string             `json:"title"`
+	Description      string             `json:"description"`
+	CreatedBy        primitive.ObjectID `json:"created_by" bson:"created_by"`
+	CreatedDate      time.Time          `json:"created_date"`
+	LastModifiedDate time.Time          `json:"last_modified_date"`
+	Type             string             `json:"type"`
+	Status           string             `json:"status"`
+	Content          string             `json:"content"`
+	FilePath         string             `json:"file_path,omitempty"`
+}
